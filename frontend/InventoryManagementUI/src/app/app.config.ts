9aa70cd9c3e525491293360014925bf6f129a58a
Kahan2607 <kahan2607@gmail.com>
2025-03-16 18:50:33 +0530

    added auth0 service components and packages along with the required setting.

--- frontend/InventoryManagementUI/src/app/app.config.ts
+++ frontend/InventoryManagementUI/src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),   
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'kahan2607.jp.auth0.com',
+      clientId: '8BjHg211CN1WrngcXMO0IAB9XVkPf7fW',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ]
+
 };

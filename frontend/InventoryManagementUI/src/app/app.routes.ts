691c35abe947b2a6e182125ec138f3f1eb2b1488
Kahan2607 <kahan2607@gmail.com>
2025-03-17 10:39:30 +0530

    added the lazy loading in the routes.ts and added empty header component

--- frontend/InventoryManagementUI/src/app/app.routes.ts
+++ frontend/InventoryManagementUI/src/app/app.routes.ts
@@ -4,6 +4,18 @@ import { AuthGuard } from '@auth0/auth0-angular';
 import { Auth0Component } from './auth0/auth0.component';
 
 export const routes: Routes = [
-    {path: '', component: Auth0Component},
-    {path: 'home', component: HomeComponent},
+    {
+        path: '', 
+        loadComponent: () => {
+            return import('./auth0/auth0.component').then(m => m.Auth0Component); // lazy loading of the auth0 component
+        }
+
+    },
+    {
+        path: 'home', 
+        loadComponent: () => {
+            return import('./components/home/home.component').then(m => m.HomeComponent); // lazy loading
+        } 
+
+    },
 ];
